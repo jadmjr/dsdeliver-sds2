aaca8fbcc41b3abb731565579934f62b306fbaa2
Julimar <jadmjr@gmail.com>
2021-01-06 16:19:06 -0300

    add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import Counter from './Counter'
+import NavBar from './Navbar';
+
 
 function App() {
   return (
     <div>
-     <h1>Hello</h1>
+    <NavBar />
     </div>
   );
 }
