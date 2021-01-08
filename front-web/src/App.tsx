e3d4823e74f07e084ba345a0bc5ae6efda6f392b
Julimar <jadmjr@gmail.com>
2021-01-08 20:32:13 -0300

    finish front web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Routes from './Routes';
+import React from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
   return (
     <>   
     <Routes />
+    <ToastContainer />
     </>
   );
 }
